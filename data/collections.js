76797a51a284b68feaf3a0078fa1c616fd3182c0
Michael Wautier <wautiermichael@gmail.com>
2020-12-04 15:16:47 +0100

    data initialized

---
+++ data/collections.js
@@ -0,0 +1,40 @@
+const collections = [
+  {
+    title: "Etoile Sud",
+    category: "photography"
+  },
+  {
+    title: "Table à manger",
+    category: "photography"
+  },
+  {
+    title: "Banal Exotisme",
+    category: "photography"
+  },
+  {
+    title: "Entre deux eaux",
+    category: "photography"
+  },
+  {
+    title: "Vavang'",
+    category: "photography"
+  },
+  {
+    title: "Humans",
+    category: "photography"
+  },
+  {
+    title: "Reveria",
+    category: "mixed"
+  },
+  {
+    title: "Slash",
+    category: "graphic"
+  },
+  {
+    title: "Mixed Visual",
+    category: "graphic"
+  },
+]
+
+export default collections;
